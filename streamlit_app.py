1bcae4e0e51d08b3efbe93cff6ff156e9f5ee852
litingh374 <litingh@gmail.com>
2025-12-22 14:54:22 +0800

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,19 @@
-<div class="row mb-3">
-    <div class="col-md-4">
-        <label class="form-label">基地面積 (m²)</label>
-        </div>
+import streamlit as st
 
-    <div class="col-md-4">
-        <label class="form-label">總樓地板面積 (m²)</label>
-        </div>
+# 這才是 Streamlit (Python) 的寫法
+col1, col2 = st.columns(2)
+with col1:
+    base_area = st.number_input("基地面積 (m²)", value=1990.00)
+with col2:
+    floor_area = st.number_input("總樓地板面積 (m²)", value=29671.96)
 
-    </div>
-
-<h5 class="mt-4"><i class="bi bi-building"></i> 地上/地下層數設定</h5> <div class="row mb-3">
-    <div class="col-md-4">
-        <label class="form-label">地上層數 (F)</label>
-        <div class="input-group">
-             </div>
-    </div>
-
-    <div class="col-md-4">
-        <label class="form-label">屋突層數 (R)</label>
-        <div class="input-group">
-            </div>
-    </div>
-
-    <div class="col-md-4">
-        <label class="form-label">地下層數 (B)</label>
-        <div class="input-group">
-            <button class="btn btn-outline-secondary" type="button" @click="decrement('basementFloors')">-</button>
-            <input type="number" class="form-control text-center" v-model.number="projectData.basementFloors">
-            <button class="btn btn-outline-secondary" type="button" @click="increment('basementFloors')">+</button>
-        </div>
-        <div class="form-check mt-2">
-            <input class="form-check-input" type="checkbox" id="soilControl" v-model="projectData.soilControl">
-            <label class="form-check-label" for="soilControl">
-                評估土方運棄管制?
-            </label>
-        </div>
-    </div>
-</div>
+st.subheader("層數設定")
+c1, c2, c3 = st.columns(3)
+with c1:
+    ground_floors = st.number_input("地上層數 (F)", value=24)
+with c2:
+    roof_floors = st.number_input("屋突層數 (R)", value=2)
+with c3:
+    # 把地下層數移到這裡
+    basement_floors = st.number_input("地下層數 (B)", value=6)
+    soil_control = st.checkbox("評估土方運棄管制?")
